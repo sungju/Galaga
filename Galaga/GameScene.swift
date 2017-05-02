7fbe14006f923f6f31ae9be263aeacd9aef341a8
Sungju Kwon <sungju.kwon@gmail.com>
2017-05-02 16:39:19 +1000

    Initial version

--- Galaga/GameScene.swift
+++ Galaga/GameScene.swift
@@ -11,79 +11,91 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var battleShip: SKSpriteNode?
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var dt : TimeInterval = 0
+    private let shipMovePointsPerSec: CGFloat = 300.0
+    private var velocity = CGPoint.zero
+    private var targetLocation = CGPoint.zero
+    
     
     override func didMove(to view: SKView) {
+        battleShip = SKSpriteNode.init(imageNamed: "Spaceship")
+        battleShip?.xScale = 0.2
+        battleShip?.yScale = 0.2
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        battleShip?.position = CGPoint(x: 0, y: -(self.size.height / 4))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        addChild(battleShip!)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func correctTargetLocation(touchLocation: CGPoint) {
+        let offset = CGPoint(x: touchLocation.x - (battleShip?.position.x)!,
+                             y: touchLocation.y - (battleShip?.position.y)!)
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        let direction = CGPoint(x: offset.x / CGFloat(length),
+                                y: offset.y / CGFloat(length))
+        
+        velocity = CGPoint(x: direction.x * shipMovePointsPerSec,
+                           y: direction.y * shipMovePointsPerSec)
+        
+        targetLocation = touchLocation
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    func sceneTouched(touches: Set<UITouch>) {
+        guard let touch = touches.first else {
+            return
         }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let touchLocation = touch.location(in: self)
+        correctTargetLocation(touchLocation: touchLocation)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        sceneTouched(touches: touches)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        sceneTouched(touches: touches)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        sceneTouched(touches: touches)
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if (lastUpdateTime > 0) {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        
+        let oldShipPosition = battleShip?.position
+        self.moveObject(node: battleShip!, velocity: velocity, dt: dt)
+        let minX = min(CGFloat((oldShipPosition?.x)!), CGFloat((battleShip?.position.x)!))
+        let minY = min(CGFloat((oldShipPosition?.y)!), CGFloat((battleShip?.position.y)!))
+        let maxX = max(CGFloat((oldShipPosition?.x)!), CGFloat((battleShip?.position.x)!))
+        let maxY = max(CGFloat((oldShipPosition?.y)!), CGFloat((battleShip?.position.y)!))
+        
+        if (targetLocation.x > minX && targetLocation.x < maxX) {
+            if (targetLocation.y > minY && targetLocation.y < maxY) {
+                battleShip?.position = targetLocation
+                velocity = CGPoint.zero
+            }
+        }
     }
+    
+    func moveObject(node: SKSpriteNode, velocity: CGPoint, dt: TimeInterval) {
+        let amountToMove = CGPoint.init(x: (velocity.x * CGFloat(dt)),
+                                        y: (velocity.y * CGFloat(dt)))
+        
+        node.position = CGPoint.init(x: node.position.x + amountToMove.x,
+                                     y: node.position.y + amountToMove.y)
+    }
+    
 }
